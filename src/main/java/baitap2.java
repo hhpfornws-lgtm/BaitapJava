60833b8cd9fd5e981323d216c3d91bb1bfe3f89f
hhpfornws-lgtm <hhpfornws@gmail.com>
2025-08-20 13:57:09 +0700

    Update bai tap moi

--- src/main/java/baitap2.java
+++ src/main/java/baitap2.java
@@ -2,21 +2,21 @@ import java.util.Scanner;
 
 public class baitap2 {
     public static void main(String[] args) {
-    // Tạo đối tượng Scanner để nhập dữ liệu từ bàn phím
-    Scanner scanner = new Scanner(System.in);
-    // Nhập chiều dài
+        // Tạo đối tượng Scanner để nhập dữ liệu từ bàn phím
+        Scanner scanner = new Scanner(System.in);
+        // Nhập chiều dài
 
-    System.out.print("Nhập chiều dài: ");
-    double length = scanner.nextDouble();
-    // Nhập chiều rộng
+        System.out.print("Nhập chiều dài: ");
+        double length = scanner.nextDouble();
+        // Nhập chiều rộng
 
-    System.out.print("Nhập chiều rộng: ");
-    double width = scanner.nextDouble();
+        System.out.print("Nhập chiều rộng: ");
+        double width = scanner.nextDouble();
 
-    // Tính diện tích
-    double area = length * width;
+        // Tính diện tích
+        double area = length * width;
 
-    // In kết quả
-    System.out.println("Diện tích hình chữ nhật là: " + area);
+        // In kết quả
+        System.out.println("Diện tích hình chữ nhật là: " + area);
     }
 }
